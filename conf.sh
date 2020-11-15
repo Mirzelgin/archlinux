2b3ce4d4376df567cb2ab04c7fbf8aee10769c55
Mirzelgin <dubinin.lexa@yandex.ru>
2020-11-16 00:18:44 +0300

    Update conf.sh

--- conf.sh
+++ conf.sh
@@ -19,3 +19,15 @@ echo 'LANG="ru_RU.UTF-8"' > /etc/locale.conf
 ## Устанавливаем keymap и шрифт для консоли
 echo 'KEYMAP=ru' >> /etc/vconsole.conf
 echo 'FONT=cyr-sun16' >> /etc/vconsole.conf
+
+# Пароль root
+passwd
+
+pacman -Sy networkmanager network-manager-applet wireless_tools wpa_supplicant dialog os-prober mtools reflector
+
+# Установка загрузчика
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# Включаем NetworkManager
+systemctl enable NetworkManager
