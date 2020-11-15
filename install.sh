43b1cb643c5d534fd58ae17e109e5e82e54800ef
Mirzelgin <dubinin.lexa@yandex.ru>
2020-11-15 23:53:45 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Скрипт установки Arch Linux efi
+dev="/dev/sda"
 
 # Устанавливаем русскую раскладку клавиатуры (на сессию)
 loadkeys ru
@@ -12,30 +13,42 @@ setfont cyr-sun16
 timedatectl set-ntp true
 
 # Устанавливаем утилиты для работы с btrfs
-#pacman -Sy btrfs-progs
+pacman -Sy btrfs-progs
 
 # Разметка диска (-z обнуление таблицы разделов)
-cfdisk -z /dev/sda
+cfdisk -z $dev
+
+## efi раздел
+mkfs.fat -F32 -f $dev'1'
+
+## swap раздел
+mkswap $dev'2'
+swapon $dev'2'
 
 ## root раздел
-mkfs.btrfs -L "root" /dev/sda1
+mkfs.btrfs -f $dev'3'
 
 # Монтируем раздел
-mount /dev/sda1 /mnt
+mount $dev'3' /mnt
 
 # Создаём два подтома под корень и домашние каталоги
 btrfs subvolume create /mnt/@
 btrfs subvolume create /mnt/@home
+btrfs subvolume create /mnt/@var
+btrfs subvolume create /mnt/@snapshots
 
 # Отмонтируем корень ФС
 umount /mnt
 
 # Монтируем корневой раздел (c включенным TRIM)
-mount -o subvol=@,compress=zstd discard=async /dev/sda1 /mnt
+mount -o noatime,compress=zstd,subvol=@ discard=async $dev'3' /mnt
 
-# Создаём директорию home и монтируем в неё subvolume @home
-mkdir /mnt/home
-mount -o subvol=@home,compress=zstd discard=async /dev/sda1 /mnt/home
+# Создаём директории home, var, .snapshots и монтируем соответствуюущие subvolume
+mkdir -p /mnt/{boot,home,var,.snapshots}
+mount $dev'1' /mnt/boot
+mount -o noatime,compress=zstd,subvol=@home discard=async $dev'3' /mnt/home
+mount -o noatime,compress=zstd,subvol=@var discard=async $dev'3' /mnt/var
+mount -o noatime,compress=zstd,subvol=@snapshots discard=async $dev'3' /mnt/.snapshots
 
 # Обновляем зеркала
 ## Создаём резервную копию списка зеркал
@@ -51,7 +64,7 @@ reflector --verbose  -l 5 -p https --sort rate --save /etc/pacman.d/mirrorlist
 pacman -Syyu
 
 # Установка системы
-pacstrap /mnt base base-devel linux linux-firmware vim btrfs-progs grub-btrfs efibootmgr
+pacstrap /mnt base base-devel linux linux-firmware linux-headers btrfs-progs grub grub-btrfs efibootmgr vim
 
 # Генерируем fstab
 genfstab -U /mnt >> /mnt/etc/fstab
@@ -81,15 +94,14 @@ echo 'LANG="ru_RU.UTF-8"' > /etc/locale.conf
 echo 'KEYMAP=ru' >> /etc/vconsole.conf
 echo 'FONT=cyr-sun16' >> /etc/vconsole.conf
 
-# Initramfs
-vim /etc/mkinitcpio.conf
-# В файле /etc/mkinitcpio.conf необходимо отредактировать строку
-# HOOKS=(base udev autodetect modconf block filesystems keyboard fsck)
-# и убрать из неё fsck
-mkinitcpio -P
-
 # Пароль root
 passwd
 
+pacman -Sy networkmanager network-manager-applet wireless_tools wpa_supplicant dialog os-prober mtools reflector
+
 # Установка загрузчика
-grub-install /dev/sda
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# Включаем NetworkManager
+systemctl enable NetworkManager
