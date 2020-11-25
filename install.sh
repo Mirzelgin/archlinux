f71b78e315a3d437a51655917af7bfa80b14810a
Mirzelgin <dubinin.lexa@yandex.ru>
2020-11-25 22:25:37 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -71,35 +71,3 @@ genfstab -U /mnt >> /mnt/etc/fstab
 
 # Chroot
 arch-chroot /mnt /bin/bash
-
-# Имя компьютера
-echo "arch-mir" > /etc/hostname
-
-# Настройка часового пояса
-ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
-hwclock --systohc
-
-# Локализация
-## Устанавливаем английскую и русскую локали
-sed -i "s/#\(en_US\.UTF-8\)/\1/" /etc/locale.gen
-sed -i "s/#\(ru_RU\.UTF-8\)/\1/" /etc/locale.gen
-
-## Обновляем текущую локаль
-locale-gen
-
-## Устанавливаем русский язык системы
-echo 'LANG="ru_RU.UTF-8"' > /etc/locale.conf
-
-## Устанавливаем keymap и шрифт для консоли
-echo 'KEYMAP=ru' >> /etc/vconsole.conf
-echo 'FONT=cyr-sun16' >> /etc/vconsole.conf
-
-# Пароль root
-passwd
-
-# Установка загрузчика
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
-grub-mkconfig -o /boot/grub/grub.cfg
-
-# Включаем NetworkManager
-systemctl enable NetworkManager
